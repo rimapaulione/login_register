4e7012635562883ad68b8c55839b3f288e70cc7b
rimapaulione <rimapaulione@gmail.com>
2024-11-25 15:18:21 +0200

    change layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,8 @@
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-full flex items-center justify-center">{children}</div>
+    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-400 to-slate-800">
+      {children}
+    </div>
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ const font = Poppins({
 
 export default function Home() {
   return (
-    <main>
-      <div className="space-y-6 text-center">
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-400 to-slate-800">
+      <div className=" space-y-6 text-center ">
         <div
           className={cn(
             "text-6xl font-semibold text-white drop-shadow-md flex items-center gap-4 justify-center",
